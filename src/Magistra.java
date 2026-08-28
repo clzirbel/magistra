c3e119850902bbe87ce9af3d197270482a8d628a
Craig L. Zirbel <zirbel@bgsu.edu>
2026-08-27 22:26:07 -0700

    Add practice percentage

--- src/Magistra.java
+++ src/Magistra.java
@@ -1,31 +1,33 @@
 import java.io.File;
 
-// Magistra.java 
+// Magistra.java
 
 class Magistra {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
     	// args[0] is the language
     	// args[1] is the user's name
     	// args[2] tells what group to start with, and other things
-    	// args[3] tells where the dictionary files are stored
-    	// args[4] tells where the user files are stored
-    	
+		// args[3] tells what percentage of known words to practice
+		// args[4] tells where the dictionary files are stored
+    	// args[5] tells where the user files are stored
+
     	Magistra m = new Magistra();
 
-    	m.run(args[0], args[1], args[2], args[3], args[4]);
+    	m.run(args[0], args[1], args[2], args[3], args[4], args[5]);
     }
-    
-    public void run(String name, String language, String levelText, String dictionarypath, String userfilepath)
+
+    public void run(String name, String language, String levelText, String percentageText, String dictionarypath, String userfilepath)
     {
 
     System.out.println("Magistra was called with:");
-    System.out.println("Language:  " + language);
-    System.out.println("Name    :  " + name);
-    System.out.println("Level   :  " + levelText);
-    System.out.println("DPath   :  " + dictionarypath);
-    System.out.println("UPath   :  " + userfilepath);
-    	
+    System.out.println("Language  :  " + language);
+    System.out.println("Name      :  " + name);
+    System.out.println("Level     :  " + levelText);
+	System.out.println("Percentage:  " + percentageText);
+    System.out.println("DPath     :  " + dictionarypath);
+    System.out.println("UPath     :  " + userfilepath);
+
 	int i, numSelected, PriorityType;
 
 	int level;
@@ -33,12 +35,21 @@ class Magistra {
 		level = Integer.parseInt(levelText);
 	}
 	catch(NumberFormatException e) {
-		System.out.println("Using level 0");
-		level = 0;
+		System.out.println("Starting with group 1");
+		level = 1;
+	}
+
+	int percentage;
+	try {
+		percentage = Integer.parseInt(percentageText);
+	}
+	catch(NumberFormatException e) {
+		System.out.println("Using percentage 25");
+		percentage = 25;
 	}
 
 	String wordlistfile;
-	
+
 	if (dictionarypath.length() == 0)
 		wordlistfile = "pair_lists" + File.separator + language+".txt";
 	else
@@ -49,14 +60,14 @@ class Magistra {
 	System.out.println("Read " + wordlistfile);
 
 	String userdatafile;
-	
+
 	if (userfilepath.length() == 0)
 		userdatafile = name + "-"+language+".txt";
 	else
 		userdatafile = userfilepath + File.separator + name + "-"+language+".txt";
 
 	UserData UD = new UserData(userdatafile);
-	    
+
 	WL.incorporateUserData(UD);
 
 	String[] Selected = new String[9999];
@@ -66,7 +77,7 @@ class Magistra {
 
 	WL.setSelected(Selected, numSelected);
 
-    WL.setPriorities(level);
+    WL.setPriorities2025(level,percentage);  // new way of prioritizing the word pairs
     WL.setOrder(numSelected);
 
     Practice P = new Practice(WL);
